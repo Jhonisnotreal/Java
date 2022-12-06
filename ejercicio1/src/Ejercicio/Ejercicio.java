488b9b868dcb039fff0472c8cb32338d5824a8dc
JhonIsNotReal <110693901+Jhonisnotreal@users.noreply.github.com>
2022-12-06 00:11:54 -0600

    Add files via upload

---
+++ ejercicio1/src/Ejercicio/Ejercicio.java
@@ -0,0 +1,20 @@
+package Ejercicio;
+
+public class Ejercicio {
+    public static void main(String[] args) {
+        int numero = 12;
+        System.out.println("El dato es un int: " + numero);
+
+        long numero2 = 120432;
+        System.out.println("El dato es un long: " + numero2);
+
+        double numeroDecimal = 123.42d;
+        System.out.println("El dato es un double: " +  numeroDecimal);
+
+        String palabras = "Hola Planeta Tierra";
+        System.out.println(("El dato es un String: " + palabras));
+
+        boolean definicion = true;
+        System.out.println("El dato es un boolenao: " + definicion);
+    }
+}
